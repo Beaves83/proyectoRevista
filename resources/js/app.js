ad9e73e81e36058a56283901b2d4c402d98f62dc
Bibiano Ruiz Crespo <brc61793@dominio.es>
2019-09-19 11:22:01 +0200

    vue.js instalado y funcionando.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
-//Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-var app = new Vue({
-    el: '#app',
-    data: {
-    message: 'Hello Vue.js!'
-  }
-});
